c952aaf129c83089d8c5b90e6e8405e1a218e67c
sotoalexs901 <sotoalexs90@gmail.com>
2025-12-12 12:23:10 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -20,5 +20,5 @@ export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
 
-// ✅ MUST match functions/index.js region
-export const functions = getFunctions(app, "us-east1");
+// ✅ IMPORTANT: must match functions region
+export const functions = getFunctions(app, "us-east4");
