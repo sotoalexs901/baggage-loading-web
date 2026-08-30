c13c8d6cb150b7e6fd1777d38bd3b429b6d9e06d
sotoalexs901 <sotoalexs90@gmail.com>
2026-08-29 20:58:02 -0400

    Add service worker for caching and offline support

---
+++ public/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "blcs-v1";
+
+const APP_SHELL = [
+  "/",
+  "/manifest.webmanifest",
+  "/blcs-icon-192.png",
+  "/blcs-icon-512.png",
+  "/apple-touch-icon.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
+      )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
